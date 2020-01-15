e102cc539d753c203d99a7b646506d955257d751
Piyush <piyush.kotian@gmail.com>
2020-01-15 19:44:18 +0530

    Added computeRequestHash lambda function

---
+++ amplify/backend/function/computeHash/src/index.js
@@ -0,0 +1,60 @@
+var crypto = require("crypto")
+
+exports.handler = async event => {
+  // hashSequence =
+  // key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5||||||salt;
+  let req = event["req"]
+
+  // Get important data from given string
+  const isImpDataMissing = "key|txnid|amount|productinfo|firstname|email"
+    .split("|")
+    .map(item => [undefined, null].includes(req[item]))
+    .includes(true)
+
+  // Check if important data is missing
+  if (isImpDataMissing) {
+    console.log("Important data is missing")
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        "Error: Missing critical component(s) required to generate hashing sequence"
+      ),
+    }
+  }
+
+  // One blank must be left even if no UDF is supplied
+  // Therefore, check if udf is empty
+  const isUdfEmpty = ![1, 2, 3, 4, 5]
+    .map(item => [undefined, null].includes(req["udf" + item.toString()]))
+    .includes(false)
+
+  // Replace one udf with blank (required as per Docs)
+  if (isUdfEmpty) {
+    req["udf1"] = ""
+  }
+
+  // Generate hashing sequence
+  const salt = "QZpaSP4aQk"
+  let hashingSequence =
+    "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5"
+      .split("|")
+      .map(item => req[item])
+      .filter(item => item !== undefined)
+      .join("|") +
+    "||||||" +
+    salt
+
+  // Compute request hash
+  const hash = crypto
+    .createHash("sha512")
+    .update(hashingSequence)
+    .digest("hex")
+
+  const response = {
+    statusCode: 200,
+    body: {
+      hash: hash,
+    },
+  }
+  return response
+}
