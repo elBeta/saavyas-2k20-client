610a8309fe3df21042479b76582ed81a14525086
Piyush <piyush.kotian@gmail.com>
2020-01-17 19:53:38 +0530

    Fixed bolt.launch call

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -10,18 +10,45 @@ function RegistrationPage() {
     const path = "/request-hash"
     const init = {
       queryStringParameters: {
-        key: "abc",
-        txnid: "xyz",
-        amount: "123",
-        productinfo: "best",
-        firstname: "jay",
-        email: "nomail",
-        hash: "shash",
+        key: "T5g199Dn",
+        txnid: "tid1234",
+        amount: 17,
+        productinfo: "TestProduct",
+        firstname: "Piyush",
+        email: "piyush.kotian@gmail.com",
       },
     }
     Amplify.configure(awsconfig)
     API.get(apiName, path, init)
-      .then(response => console.log(response))
+      .then(response => {
+        console.log(response)
+        return response
+      })
+      .then(resp => {
+        console.log(resp)
+        window.bolt.launch(
+          {
+            key: "T5g199Dn",
+            txnid: "tid1234",
+            amount: 17,
+            productinfo: "TestProduct",
+            firstname: "Piyush",
+            email: "piyush.kotian@gmail.com",
+            phone: "7338500594",
+            surl: "localhost:8000/registration",
+            furl: "localhost:8000/registration",
+            hash: resp,
+          },
+          {
+            resonseHandler: BOLT => {
+              console.log(BOLT.response.txnStatus)
+            },
+            catchException: function(BOLT) {
+              alert(BOLT.message)
+            },
+          }
+        )
+      })
       .catch(err => console.log(err))
   }, [])
 
