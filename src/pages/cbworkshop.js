c44e00388ea58a65fc5eaf64687ac4fb812cb1c2
Piyush <piyush.kotian@gmail.com>
2020-01-22 20:58:15 +0530

    Removed blender2020 link and added cbworkshop link to index.js

---
+++ src/pages/cbworkshop.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react"
+
+function CBWorkshopPage() {
+  useEffect(() => {
+    window.location = "https://saavyas.typeform.com/to/wL2RTR"
+  })
+
+  return null
+}
+
+export default CBWorkshopPage
