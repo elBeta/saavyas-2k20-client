55a0bf3a2d91e82ce25af333f0404e6ce95bbfa9
Piyush <piyush.kotian@gmail.com>
2020-02-03 18:52:27 +0530

    Added back button to technical events page and hidden overflowX property

--- src/pages/events/technical.js
+++ src/pages/events/technical.js
@@ -1,6 +1,9 @@
 import React from "react"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { makeStyles } from "@material-ui/core/styles"
+import Fab from "@material-ui/core/Fab"
+import BackIcon from "@material-ui/icons/ArrowBack"
+import { navigate } from "gatsby"
 
 import EventSlide from "../../components/events/EventSlide"
 import CategorySelector from "../../components/events/CategorySelector"
@@ -13,18 +16,36 @@ const useStyles = makeStyles(theme => ({
   root: {
     height: "100%",
     width: "100%",
+    overflowX: "hidden",
   },
   highlightEvents: {
     height: "60vh",
+  },
+  backBtn: {
+    position: "fixed",
+    zIndex: 100,
+    top: "1rem",
+    left: "1rem",
   },
 }))
 
 function EventsPage(props) {
   const classes = useStyles()
 
+  const handleBackBtnClick = e => {
+    navigate("/events")
+  }
+
   return (
     <>
       <CssBaseline />
+      <Fab
+        color="primary"
+        className={classes.backBtn}
+        onClick={handleBackBtnClick}
+      >
+        <BackIcon />
+      </Fab>
       <div className={classes.root}>
         <div className={classes.highlightEvents}>
           <CategorySelector categories={highlightEvents} />
