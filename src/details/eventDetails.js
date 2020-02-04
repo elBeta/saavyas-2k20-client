3cff32800c44328b2f749a08e5cc49892cbb9891
Piyush <piyush.kotian@gmail.com>
2020-02-04 18:47:50 +0530

    Added GDG ML and Web dev technical event

--- src/details/eventDetails.js
+++ src/details/eventDetails.js
@@ -51,25 +51,23 @@ export const highlightEvents = [
 
 export const events = [
   {
-    id: "iotwithgass",
-    titleA: "IoT with",
-    titleB: "Google Assistant",
-    rightAlign: false,
+    id: "gdg_ml_wd",
+    titleA: "ML and Web Dev",
+    titleB: "By Team GDG",
     colorScheme: colorScheme.blueWhiteBlack,
-    eventDate: "6th and 7th March",
-    eventFee: 1500,
+    imageFileName: "gdg_ml_wd.png",
+    eventDate: "9th February 2020",
+    eventFee: "Free",
+    eventTime: "10:00 AM to 1:00 PM",
     eventSummary:
-      "Internet of Things (IoT) is an ecosystem of connected physical " +
-      "things that are accessible through the internet. The 'Thing' in IoT " +
-      "could be a person with a heart monitor or an automobile with " +
-      "built-in-sensors, i.e. objects that have been assigned an IP address " +
-      "and could collect and transfer data over a network without manual " +
-      "intervention.",
-    ruleBookLink:
-      "https://drive.google.com/open?id=1_B4qjVQCczijVXZO0tP-5Rmr1xZd61bA",
-    registrationLink:
-      "https://www.payumoney.com/paybypayumoney/#/FDD5FAEB0B834D653EA32AE59CC4DB14",
+      "Saavyas'20 and GDG Goa present you a fun way to spend your Sunday, " +
+      "a workshop on Machine Learning and Web development, topics that are " +
+      "omnipresent in every our every modern day routine - By Team GDG. " +
+      "Rudra Sawant - Intro to ML and DL; " +
+      "Salil Naik - Being a creative front-end developer; " +
+      "Ashley D'Souza - Design Thinking; ",
   },
+
   {
     id: "humanrobot",
     titleA: "Humanoid",
@@ -108,6 +106,26 @@ export const events = [
       "https://www.payumoney.com/paybypayumoney/#/FDD5FAEB0B834D653EA32AE59CC4DB14",
   },
   {
+    id: "iotwithgass",
+    titleA: "IoT with",
+    titleB: "Google Assistant",
+    rightAlign: false,
+    colorScheme: colorScheme.blueWhiteBlack,
+    eventDate: "6th and 7th March",
+    eventFee: 1500,
+    eventSummary:
+      "Internet of Things (IoT) is an ecosystem of connected physical " +
+      "things that are accessible through the internet. The 'Thing' in IoT " +
+      "could be a person with a heart monitor or an automobile with " +
+      "built-in-sensors, i.e. objects that have been assigned an IP address " +
+      "and could collect and transfer data over a network without manual " +
+      "intervention.",
+    ruleBookLink:
+      "https://drive.google.com/open?id=1_B4qjVQCczijVXZO0tP-5Rmr1xZd61bA",
+    registrationLink:
+      "https://www.payumoney.com/paybypayumoney/#/FDD5FAEB0B834D653EA32AE59CC4DB14",
+  },
+  {
     id: "robowars",
     titleA: "Robo",
     titleB: "Wars",
@@ -115,11 +133,7 @@ export const events = [
     eventFee: "...",
     imageFileName: "robowars.jpg",
     rightAlign: true,
-    eventSummary:
-      "Humanoid robots are being developed to perform human tasks like " +
-      "personal assistance, through which they should be able to assist " +
-      "the sick and elderly, and dirty or dangerous jobs. The robotic " +
-      "industry will see an increase in humanoid robots.",
+    eventSummary: "Self explanatory....",
     colorScheme: colorScheme.redBlackWhite,
   },
 ]
