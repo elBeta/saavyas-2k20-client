24c19f8d295852b415688cab371720bafe817d69
Piyush <piyush.kotian@gmail.com>
2020-02-08 00:17:23 +0530

    Added team member names to meet the team page

--- src/details/teamDetails.js
+++ src/details/teamDetails.js
@@ -4,17 +4,77 @@ export const teamDetails = [
     members: [
       { name: "Aashay", phoneNo: "...", emailId: "..." },
       { name: "Devanshi", phoneNo: "...", emailId: "..." },
+      { name: "Damodar", phoneNo: "...", emailId: "..." },
+      { name: "Nihal", phoneNo: "...", emailId: "..." },
     ],
   },
   {
-    name: "#Events-Team",
+    name: "#Event-Managers",
     members: [
       { name: "Vikas", phoneNo: "...", emailId: "..." },
       { name: "Kaksha", phoneNo: "...", emailId: "..." },
+      { name: "Poonam", phoneNo: "...", emailId: "..." },
+      { name: "Prashant", phoneNo: "...", emailId: "..." },
     ],
   },
   {
-    name: "#Tech-Team",
+    name: "#Sponsorship-Heads",
+    members: [
+      { name: "Ashutosh", phoneNo: "...", emailId: "..." },
+      { name: "Rahul", phoneNo: "...", emailId: "..." },
+      { name: "Shrenik", phoneNo: "...", emailId: "..." },
+    ]
+  },
+  {
+    name: "#Finance-Head",
+    members: [
+      { name: "Prince", phoneNo: "...", emailId: "..." },
+    ]
+  },
+  {
+    name: "#Marketing-Head",
+    members: [
+      { name: "Archit", phoneNo: "...", emailId: "..." },
+      { name: "Richa", phoneNo: "...", emailId: "..." },
+      { name: "Rakesh", phoneNo: "...", emailId: "..." },
+    ]
+  },
+  {
+    name: "#Design-Head",
+    members: [
+      { name: "Rajit", phoneNo: "...", emailId: "..." },
+      { name: "Aparimeet", phoneNo: "...", emailId: "..." },
+    ]
+  },
+  {
+    name: "#Photography-Head",
+    members: [{ name: "Soaib", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Tech-Head",
     members: [{ name: "Piyush", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Hospitality-Head",
+    members: [{ name: "Kaustubh", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Pronites-Head",
+    members: [{ name: "Abhishek", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Operations-Head",
+    members: [{ name: "Akash", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Workshops-Head",
+    members: [{ name: "Pranav", phoneNo: "...", emailId: "..." }],
+  },
+  {
+    name: "#Documentation-Head",
+    members: [
+      { name: "Sireeja", phoneNo: "...", emailId: "..." },
+      { name: "Pratyay", phoneNo: "...", emailId: "..." },
+    ]
   },
 ]
