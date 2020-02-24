e8778b94f20810e268da1e7e86c76b923765204f
Piyush <piyush.kotian@gmail.com>
2020-02-24 19:10:53 +0530

    elBeta

--- src/details/sportsEventsDetails.js
+++ src/details/sportsEventsDetails.js
@@ -9,7 +9,8 @@ export const events = [
     eventTime: "10:30 AM",
     eventZone: "Alpha",
     eventLocation: "N2/N4",
-    eventPrize: "Rs 11k",
+    eventPrize: "Rs 7k",
+    eventFee: 100,
     eventSummary:
       "Wanna a play of game of strategy? Impress everyone by showcasing your intelligence!",
   },
@@ -21,7 +22,8 @@ export const events = [
     eventTime: "10:30 AM",
     eventZone: "Bravo",
     eventLocation: "N5/N6",
-    eventPrize: "Rs 7.5k",
+    eventPrize: "Rs 4.5k",
+    eventFee: 50,
     eventSummary:
       "Smash those balls across the court and get those amazing points!",
   },
@@ -34,6 +36,7 @@ export const events = [
     eventZone: "Echo",
     eventLocation: "Ground Sector 2",
     eventPrize: "Rs 24k",
+    eventFee: 1000,
     eventSummary:
       "Smash those balls across the court and get those amazing points!",
   },
@@ -45,18 +48,21 @@ export const events = [
     eventTime: "12:00 PM",
     eventZone: "Echo",
     eventLocation: "Ground Sector 2",
-    eventPrize: "Rs 6k",
+    eventPrize: "Rs 1.5k",
+    eventFee: 50,
     eventSummary:
       "Unleash your inner Dhonis and Virats! Its time to apply your best cricketing tactics to drive your team to its glory!",
   },
   {
     id: "pubg",
     titleA: "PUBG",
+    titleB: "Agent 47",
     eventDate: "8th March",
     eventTime: "2:00 PM",
     eventZone: "Bravo",
     eventLocation: "N8",
-    eventPrize: "Rs 5k",
+    eventPrize: "Rs 4.5k",
+    eventFee: 200,
     eventSummary:
       "Winner winner chicken dinner! Engage in this addicting Battle Royale to put your gaming skills to the test!",
   },
@@ -67,9 +73,19 @@ export const events = [
     eventTime: "10:30 AM",
     eventZone: "Delta",
     eventLocation: "2A/2B",
-    eventPrize: "Rs 5.5k",
+    eventPrize: "Rs 4k",
+    eventFee: 100,
     eventSummary:
       "Kick, pass, shoot and GOAALLL! Send that ball flying into your opponents post and score!",
+  },
+  {
+    id: "csgo",
+    titleA: "CS:GO",
+    eventDate: "8th March",
+    eventZone: "Delta",
+    eventPrize: "Rs 4k",
+    eventFee: 200,
+    eventSummary: "",
   },
 ].map(item => ({
   ...item,
--- src/details/technicalEventsDetails.js
+++ src/details/technicalEventsDetails.js
@@ -35,7 +35,8 @@ export const events = [
     eventTime: "01:00 PM",
     eventZone: "Golf",
     eventLocation: "Parking Lot",
-    eventPrize: "Rs 20k",
+    eventFee: 500,
+    eventPrize: "Rs 14k",
     eventSummary:
       "Drone Prix: SAAVYAS-20 presents you a robotic platform that has been successfully used to develop a competition (called Drone Challenge) in which students has to program the navigation system for a simulated unmanned aerial vehicle (or drone).",
   },
@@ -47,7 +48,8 @@ export const events = [
     eventTime: "02:00 PM",
     eventZone: "Charlie",
     eventLocation: "Badminton Court",
-    eventPrize: "Rs 12k",
+    eventFee: 400,
+    eventPrize: "Rs 10k",
     eventSummary:
       "Its football time and no-one plays soccer better than the engineers, in our own sophisticated way we hail this event ROBO SOCCER. All you soccer fans hold your breath, as SAAVYAS present to you soccer with an entirely new dimension. So if you are up for the challenge get your manual soccer playing bots ready in order to compete on the biggest platform and bring out the hidden RONALDO, MESSI in you…its time to stop watching soccer on screens. Come over during the fest and take a chance to win exciting cash prizes.",
   },
@@ -59,7 +61,17 @@ export const events = [
     eventTime: "10:00 AM",
     eventZone: "Foxtrot",
     eventLocation: "ITI Lawn",
+    eventFee: 2500,
     eventPrize: "Rs 60k",
+    eventSummary:
+      "SAAVYAS presents the Reign of Fire! One of Goa’s Largest Robot Combat Competition where participants across India battle each other for the coveted Robowars Champion Title. Witness sparks fly as metal crushes metal in the most glorious battles interspersed with magnificent artist performances for non-stop entertainment. Brace yourselves to be a part of one of the most striking event here at NIT Goa during Saavyas.",
+  },
+  {
+    id: "ard-challenge",
+    titleA: "Fireproof",
+    titleB: "Arduino Challenge",
+    eventFee: 100,
+    eventPrize: "Rs 5.5k",
     eventSummary:
       "SAAVYAS presents the Reign of Fire! One of Goa’s Largest Robot Combat Competition where participants across India battle each other for the coveted Robowars Champion Title. Witness sparks fly as metal crushes metal in the most glorious battles interspersed with magnificent artist performances for non-stop entertainment. Brace yourselves to be a part of one of the most striking event here at NIT Goa during Saavyas.",
   },
@@ -72,6 +84,7 @@ export const events = [
     eventTime: "12:00 PM",
     eventZone: "Charlie",
     eventLocation: "Badminton Court",
+    eventFee: 300,
     eventPrize: "Rs 8k",
     eventSummary:
       "Be an innovator and BLAZE A WAY. A simple event based on robotics. Build a line follower robot, which is capable of following line path which is produced in front of it. The robots should be designed to quickly follow the black line. However, they may meet different obstacles on the track – split and join of the line, objects on the track and line interruptions. Only robots that overcome all obstacles can succeed.",
@@ -84,7 +97,8 @@ export const events = [
     eventTime: "10:30 AM",
     eventZone: "Foxtrot",
     eventLocation: "ITI Lawn",
-    eventPrize: "Rs 10k",
+    eventFee: 600,
+    eventPrize: "Rs 13k",
     eventSummary:
       "More speed, more power with minimum time and perfection - Here comes the racing series for humans and artificial intelligence. The way you enter, the way you proceed, the way you reveal your speed to other beings here at the enthralling event of “Highway through Hell”. We provide you a track to open your enthusiasm and rush you to win with your bot . The fast speeds, tight turns, and exciting collisions in our hot wheels takes place on track. It is the event where you to get to test the efficiency and power, skills of your droid/robot. Race your bots till the finish line and earn accolades.",
   },
