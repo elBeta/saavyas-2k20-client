74ba57dc74881a2ba6f0b3f7b3b997857e521376
Piyush <piyush.kotian@gmail.com>
2020-02-25 09:32:37 +0530

    Added check for location.state on registration page

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -131,7 +131,7 @@ function RegistrationForm(props) {
     txnid: "",
   })
   const [isLoading, setIsLoading] = useState(false)
-  const eventID = location.state.eventID
+  const eventID = location.state ? location.state.eventID : ""
 
   // Get Form Fields from eventId
   useEffect(() => {
