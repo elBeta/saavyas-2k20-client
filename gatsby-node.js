ab78bb2f9c5e306bff35ba3fb5b923fb6a547eb2
Piyush <piyush.kotian@gmail.com>
2020-02-28 05:47:14 +0530

    Fix for modules requiring window at build time (react-qr-reader)

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+// Replace the offending module (requiring window at production) with a dummy module during server rendering.
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-qr-reader/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
